fae567c5fe93bd99ef2271b5c593047077c61423
francescomascellino <franesco.mascellino@gmail.com>
2023-11-07 15:40:23 +0100

    Initial commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<nav class="nav justify-content-center  ">
+    <a class="nav-link" href="#" aria-current="page">Home</a>
+    <a class="nav-link" href="#">About</a>
+    <a class="nav-link" href="#">Contacts</a>
+</nav>
