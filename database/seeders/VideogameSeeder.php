4dac407f7e7984f0f8a4ae5bd378791030166ce1
Francesco-Munafo <acquamarea1@gmail.com>
2023-11-07 17:58:29 +0100

    Add seeder params

--- database/seeders/VideogameSeeder.php
+++ database/seeders/VideogameSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
+use App\Models\Videogame;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class VideogameSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 8; $i++) {
+            $new_game = new Videogame();
+            $new_game->title = $faker->realText(50);
+            $new_game->image = $faker->imageUrl();
+            $new_game->price = $faker->randomFloat(2, 1, 999);
+            $new_game->release_date = $faker->dateTimeBetween('-30 years', '+3 years');
+            $new_game->developer = $faker->company();
+            $new_game->publisher = $faker->company();
+            $new_game->save();
+        }
     }
 }
